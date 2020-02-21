02e7b1d8c41aa7c6f909ed7457ec091e5830aff0
Ian Maurmann <ikmxx@yahoo.com>
2020-02-20 21:00:53 -0700

    Getting started on the Database Wrapper
    - New object, Database Wrapper object.
    - Edit to composer.json --- Added new namespace for the Database Wrapper.

---
+++ src/plugins/database-wrapper/src/PithDatabaseWrapper.php
@@ -0,0 +1,27 @@
+<?php
+# ===================================================================
+# Copyright (c) 2008-2020 Ian K Maurmann. The Pith Framework is
+# provided under the terms of the Mozilla Public License, v. 2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ===================================================================
+
+declare(strict_types=1);
+
+
+// Pith Database Wrapper for MySQL using PDO
+// -----------------------------------------
+
+namespace Pith\DatabaseWrapper;
+
+
+class PithDatabaseWrapper
+{
+    public function whereAmI()
+    {
+        return 'Pith Database Wrapper';
+    }
+
+}
