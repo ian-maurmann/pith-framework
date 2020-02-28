a915c1e048db5ccb6e1da0729e335c82075988b9
Ian Maurmann <ikmxx@yahoo.com>
2020-02-28 00:37:12 -0700

    Database Wrapper - Connect

--- src/plugins/database-wrapper/src/PithDatabaseWrapper.php
+++ src/plugins/database-wrapper/src/PithDatabaseWrapper.php
@@ -23,6 +23,9 @@ class PithDatabaseWrapper
     private $options;
     private $did_connect;
     private $pdo;
+    private $db_user_username;
+    private $db_user_password;
+    private $connection_problems;
 
     function __construct()
     {
@@ -37,17 +40,29 @@ class PithDatabaseWrapper
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES   => false,
         ];
+
+        $this->connection_problems = '';
     }
+
 
     public function setDsn($dsn)
     {
         $this->dsn = $dsn;
     }
 
+
     public function setOptions($options)
     {
         $this->options = $options;
     }
+
+
+    public function setDbUserAndPassword($db_user_username, $db_user_password)
+    {
+        $this->db_user_username = $db_user_username;
+        $this->db_user_password = $db_user_password;
+    }
+
 
     public function getStatus()
     {
@@ -59,6 +74,32 @@ class PithDatabaseWrapper
 
         return $status;
     }
+
+
+    public function connect()
+    {
+        $did_connect = true;
+        try{
+            $this->pdo = new \PDO($this->dsn, $this->db_user_username, $this->db_user_password, $this->options);
+        }
+        catch(\PDOException $exception){
+            $did_connect = false;
+            $this->connection_problems .= 'Connection failed: ' . $exception->getMessage() . '. ';
+        }
+        $this->did_connect = $did_connect;
+
+        return $did_connect;
+    }
+
+
+    public function connectOnce()
+    {
+        if(!$this->did_connect){
+            $this->connect();
+        }
+        return $this->did_connect;
+    }
+
 
     public function whereAmI()
     {
