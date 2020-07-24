7876d2c08c73884083bb698acadd558b454bc579
Ian Maurmann <ikmxx@yahoo.com>
2020-07-23 20:37:31 -0700

    Getting started on the Command Tool
    - Edits to the composer.json file --- Included Conso as a dependency.
    - New object, PithCommandTool, to use as a wrapper for Conso.
    - New executable PHP file in root folder, pith, for running cli commands using Conso.

---
+++ src/plugins/command-tool/src/PithCommandTool.php
@@ -0,0 +1,97 @@
+<?php
+# ===================================================================
+# Copyright (c) 2008-2020 Ian K Maurmann. The Pith Framework is
+# provided under the terms of the Mozilla Public License, v. 2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ===================================================================
+
+declare(strict_types=1);
+
+
+// Pith Command Tool,
+// using Conso ---> lotfio/conso
+// See: https://github.com/lotfio/conso
+// -----------------------------------------
+
+namespace Pith\CommandTool;
+
+use Conso;
+
+class PithCommandTool
+{
+    private $conso;
+
+    function __construct()
+    {
+
+    }
+
+    public function start()
+    {
+        $this->conso = new Conso\Conso(new Conso\Input, new Conso\Output);
+
+        $this->initialize();
+
+        $this->conso->run();
+    }
+
+
+
+
+
+    private function initialize()
+    {
+        $this->initializeMetadata();
+        $this->initializeSignature();
+        $this->initializeCommands();
+    }
+
+
+
+
+    private function initializeMetadata(){
+        $this->conso->setName('Pith Command Tool');
+        $this->conso->setVersion('0.0.0');
+        $this->conso->setAuthor('Ian Maurmann');
+    }
+
+
+
+
+
+    private function initializeSignature()
+    {
+        $this->conso->setSignature("\033[32m" . "\n╔════╗\n║Test║\n╚════╝\n");
+    }
+
+
+
+
+
+    private function initializeCommands()
+    {
+        $this->conso->command("test", function($input, $output){
+
+            if($input->subCommand() == 'one')
+                exit($output->writeLn("\n hello from one \n", 'yellow'));
+
+            if($input->subCommand() == 'two')
+                $output->writeLn("hello from two \n", 'green');
+
+        })->description("This is test command description :) ^^")->sub('one', 'two');
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
