d4b1c55dc987740d67a3bdaeba0f80448c47dcc7
Ian Maurmann <ikmxx@yahoo.com>
2021-02-16 17:53:21 -0700

    Restored old examples

---
+++ example/modules/example-module/controllers/FirstPartialController.php
@@ -0,0 +1,56 @@
+<?php
+# ===================================================================
+# Copyright (c) 2008-2019 Ian K Maurmann. The Pith Framework is
+# provided under the terms of the Mozilla Public License, v. 2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ===================================================================
+
+
+
+// "FirstPartialController"
+// ------------------
+
+
+
+declare(strict_types=1);
+
+
+namespace Pith\ExampleModule;
+
+
+class FirstPartialController extends \Pith\Framework\PithController implements \Pith\Framework\PithControllerInterface
+{
+    public function access()
+    {
+        $this->access_level = 'world';
+    }
+
+    public function injector($app)
+    {
+        // Load Objects
+    }
+
+    public function action($app, $objects)
+    {
+        // Set Objects
+
+        // Action Variables
+        $p = 'This is my partial';
+
+        // Push to Preparer
+        $this->prepare->p = $p;
+    }
+
+    public function preparer($app, $prepare)
+    {
+        $p = $prepare->p;
+
+        $this->view->p = $p;
+
+        $view_adapter = $app->container->get('\\Pith\\PhtmlViewAdapter\\PithPhtmlViewAdapter');
+        $this->view_adapter = $view_adapter;
+    }
+}
