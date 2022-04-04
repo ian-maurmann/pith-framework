2bba9aa641b34f43182de35c8790915a9064eea7
Ian Maurmann <ikmxx@yahoo.com>
2022-04-03 22:19:57 -0700

    Cleanup - Added PhpDoc to the App Helper

--- src/internal/objects/PithAppHelper.php
+++ src/internal/objects/PithAppHelper.php
@@ -8,26 +8,37 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ===================================================================
 
+
+/**
+ * Pith App Helper
+ * ---------------
+ *
+ * @noinspection PhpMethodNamingConventionInspection - Long method names are ok.
+ */
 declare(strict_types=1);
-
-
-// Pith App Helper
-// ---------------
 
 
 namespace Pith\Framework\Internal;
 
 
+/**
+ * Class PithAppHelper
+ * @package Pith\Framework\Internal
+ */
 class PithAppHelper
 {
-    function __construct()
+    public function __construct()
     {
         // Do nothing for now
     }
 
+
+    /**
+     * @param $app
+     */
     public function initializeDependencies($app)
     {
-        // Using secondary initialization function
+        // Using secondary initialization function // TODO remove 0.6 implementation
         $app->request_processor->init($app);
         $app->access_control->init($app);
         $app->dispatcher->init($app);
