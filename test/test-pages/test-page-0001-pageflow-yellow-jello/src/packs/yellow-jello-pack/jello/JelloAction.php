373d5dcdf922bea232a773b6248e0e25a54a9838
Ian Maurmann <ikmxx@yahoo.com>
2022-10-31 16:39:54 -0700

    Added test page from old example

---
+++ test/test-pages/test-page-0001-pageflow-yellow-jello/src/packs/yellow-jello-pack/jello/JelloAction.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Jello Action
+ * ------------
+ *
+ * @noinspection PhpPropertyNamingConventionInspection - Property names with underscores are ok.
+ * @noinspection PhpMissingParentCallCommonInspection  - Action parent methods exist as fallback.
+ * @noinspection PhpVariableNamingConventionInspection - Short variable names are ok.
+ */
+
+
+declare(strict_types=1);
+
+namespace Pith\Framework\Test\TestPage\TestPageOne\YellowJelloPack;
+
+use Pith\Framework\PithAction;
+
+/**
+ * Class JelloAction
+ * @package Pith\Framework\Test\TestPage\TestPageOne\YellowJelloPack
+ */
+class JelloAction extends PithAction
+{
+    public function __construct()
+    {
+        // Do nothing for now.
+    }
+
+    public function runAction()
+    {
+        // Variables
+        $a = 'Aaa';
+        $b = 'Bbb';
+        $c = 'Ccc';
+
+
+        // Debug
+        // echo $a;
+        // echo $b;
+        // echo $c;
+
+
+        // Push to Preparer
+        $this->prepare->a = $a;
+        $this->prepare->b = $b;
+        $this->prepare->c = $c;
+    }
+}
