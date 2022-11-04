48b8d70ac1aa861602e22ec27404dc8f05905bf4
Ian Maurmann <ikmxx@yahoo.com>
2022-11-03 22:55:09 -0700

    Added test layout - Green Layout

---
+++ test/test-pages/test-page-0003-layout-green/src/packs/green-layout-example-pack/green-layout/GreenLayoutRoute.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Green Layout Route
+ * ------------------
+ *
+ * @noinspection PhpPropertyNamingConventionInspection - Property names with underscores are ok.
+ * @noinspection PhpClassNamingConventionInspection    - Long class names are ok.
+ */
+
+
+declare(strict_types=1);
+
+namespace Pith\Framework\Test\TestPage\TestPageThree\GreenLayoutExample;
+
+use Pith\Framework\PithRoute;
+
+/**
+ * Class GreenLayoutRoute
+ * @package Pith\Framework\Test\TestPage\TestPageThree\GreenLayoutExample
+ */
+class GreenLayoutRoute extends PithRoute
+{
+    public $pack             = '\\Pith\\Framework\\Test\\TestPage\\TestPageThree\\GreenLayoutExample\\GreenLayoutExamplePack';
+    public $route_type       = 'layout';
+    public $access_level     = 'world';
+    public $view             = '[^route_folder]/green-layout-view.phtml';
+}
