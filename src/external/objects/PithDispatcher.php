5431f83366621ad864f0409d9be9353ab48572da
Ian Maurmann <ikmxx@yahoo.com>
2022-11-12 15:58:07 -0700

    Send correct headers for resources

--- src/external/objects/PithDispatcher.php
+++ src/external/objects/PithDispatcher.php
@@ -405,9 +405,11 @@ class PithDispatcher
             );
         }
 
+        // Get the base name
+        $basename = basename($real_filepath);
 
         // Don't serve dot files
-        $starts_with_dot_file = (substr(basename($real_filepath), 0, 1) === '.');
+        $starts_with_dot_file = (substr($basename, 0, 1) === '.');
         $has_sub_dot_file     = (strpos($real_filepath, DIRECTORY_SEPARATOR . '.') !== false);
         $has_dot_file         = $starts_with_dot_file || $has_sub_dot_file;
         if($has_dot_file){
@@ -415,6 +417,82 @@ class PithDispatcher
                 'Pith Framework Exception 4023: Requested Resource path includes a dot file.',
                 4023
             );
+        }
+
+        // Get extension
+        $file_extension = pathinfo($basename, PATHINFO_EXTENSION);
+
+
+        // Content types
+        $extensions_to_content_types = [
+            'apng' => 'Content-Type: image/apng',
+            'avif' => 'Content-Type: image/avif',
+            'css'  => 'Content-type: text/css; charset=utf-8',
+            'csv'  => 'Content-type: text/csv; charset=utf-8',
+            'gif'  => 'Content-Type: image/gif',
+            'html' => 'Content-type: text/html; charset=utf-8',
+            'ico'  => 'Content-Type: image/x-icon',
+            'jpeg' => 'Content-Type: image/jpeg',
+            'jpg'  => 'Content-Type: image/jpeg',
+            'js'   => 'Content-type: text/javascript; charset=utf-8',
+            'json' => 'Content-type: application/json; charset=utf-8',
+            'md'   => 'Content-type: text/markdown; charset=utf-8',
+            'mpeg' => 'Content-Type: audio/mpeg',
+            'mp4'  => 'Content-Type: video/mp4',
+            'ogg'  => 'Content-Type: application/ogg',
+            'otf'  => 'Content-Type: font/otf',
+            'pdf'  => 'Content-Type: application/pdf',
+            'png'  => 'Content-Type: image/png',
+            'svg'  => 'Content-Type: image/svg+xml',
+            'ttf'  => 'Content-Type: font/ttf',
+            'txt'  => 'Content-type: text/plain; charset=utf-8',
+            'wav'  => 'Content-Type: audio/wav',
+            'webp' => 'Content-Type: image/webp',
+            'woff' => 'Content-Type: font/woff',
+            'xml'  => 'Content-type: text/xml',
+            'zip'  => 'Content-Type: application/zip',
+        ];
+
+
+        // Set content-type headers by extension
+        if (isset($extensions_to_content_types[$file_extension])) {
+            header($extensions_to_content_types[$file_extension], true);
+        } else {
+            // Apache > 2.2.7 reported 'text/plain' or 'application/octet-stream' for unknown content types.
+            // Apache reports 'none' for files with unknown content types.
+            // Nginx reports 'text/plain' for unknown content types.
+
+            // We're going to go with 'none' here
+            header('Content-type: none; charset=utf-8', true);
+        }
+
+
+        // Extensions that need size check
+        $extensions_that_require_size = [
+            'apng',
+            'avif',
+            'gif',
+            'ico',
+            'jpeg',
+            'jpg',
+            'mpeg',
+            'mp4',
+            'ogg',
+            'otf',
+            'pdf',
+            'png',
+            'ttf',
+            'wav',
+            'webp',
+            'woff',
+            'zip',
+        ];
+
+        // Set file size headers
+        if (isset($extensions_that_require_size[$file_extension])) {
+            $file_byte_size = filesize($real_filepath);
+
+            header("Content-length: $file_byte_size");
         }
 
 
