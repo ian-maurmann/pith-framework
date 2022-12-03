a1a9c42a85b23bada32169b2eae72de81faafcdf
Ian Maurmann <ikmxx@yahoo.com>
2022-12-02 20:32:27 -0700

    View Requisitions - Specify property types

--- src/external/extend/PithViewRequisition.php
+++ src/external/extend/PithViewRequisition.php
@@ -31,9 +31,9 @@ namespace Pith\Framework;
 class PithViewRequisition extends PithWorkflowElement
 {
     public    string $element_type     = 'view-requisition';
-    public    $requisition_type = 'view-requisition';
-    protected $headers          = [];
-    protected $resources        = [];
+    public    string $requisition_type = 'view-requisition';
+    protected array  $headers          = [];
+    protected array  $resources        = [];
 
 
     public function provisionViewRequisition()
