5a59a259f7a0d7b142a3db10074f4715e184147b
Ian Maurmann <ikmxx@yahoo.com>
2023-04-15 18:56:01 -0700

    Add Green&White theme to shared infrastructure

---
+++ src/shared-infrastructure/src/shared-infrastructure-pack/footer/FooterRoute.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Home Route
+ * ----------
+ *
+ * @noinspection PhpPropertyNamingConventionInspection - Ignore.
+ */
+
+
+declare(strict_types=1);
+
+namespace Pith\Framework\SharedInfrastructure;
+
+use Pith\Framework\PithRoute;
+
+/**
+ * Class HomeRoute
+ * @package Pith\Framework\SharedInfrastructure
+ */
+class FooterRoute extends PithRoute
+{
+    public string $route_type   = 'partial';
+    public string $pack         = '\\Pith\\Framework\\SharedInfrastructure\\SharedInfrastructurePack';
+    public string $access_level = 'world';
+    public string $action       = '\\Pith\\Framework\\SharedInfrastructure\\FooterAction';
+    public string $preparer     = '\\Pith\\Framework\\SharedInfrastructure\\FooterPreparer';
+    public string $view         = '[^route_folder]/footer-view.phtml';
+}
--- src/shared-infrastructure/src/shared-theme-pack/green-and-white-theme/GreenAndWhiteLayoutViewRequisition.php
+++ src/shared-infrastructure/src/shared-theme-pack/green-and-white-theme/GreenAndWhiteLayoutViewRequisition.php
@@ -30,7 +30,16 @@ class GreenAndWhiteLayoutViewRequisition extends PithViewRequisition
         $this->addHeader('Use UTF-8 Encoding', 'Content-Type: text/html; charset=utf-8');
         $this->addHeader('Show site runs PHP 8', 'X-Powered-By: PHP/8');
 
+        // Resource roles:
+        //     0 - reset
+        //     1 - library-for-layout
+        //     2 - library-for-page
+        //     3 - library-for-partial
+        //     4 - application-for-layout
+        //     5 - application-for-page
+        //     6 - application-for-partial
+
         // Resources
-        $this->addStylesheet('Green Layout Stylesheet', '/resources/framework/shared-ui/green-layout/green-layout.css', 'application-for-layout');
+        $this->addStylesheet('Green Layout Stylesheet', '/resources/framework/shared-ui/green-and-white-theme/green-and-white-theme.css', 'application-for-layout');
     }
 }
