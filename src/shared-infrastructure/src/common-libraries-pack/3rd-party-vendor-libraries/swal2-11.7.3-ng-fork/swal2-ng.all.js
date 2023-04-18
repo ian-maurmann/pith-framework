74f0d0d95ab672ee17e20ebbe8d544bf11abd94f
Ian Maurmann <ikmxx@yahoo.com>
2023-04-17 22:47:11 -0700

    (Modify the fork's naming)

--- src/shared-infrastructure/src/common-libraries-pack/3rd-party-vendor-libraries/swal2-11.7.3-ng-fork/swal2-ng.all.js
+++ src/shared-infrastructure/src/common-libraries-pack/3rd-party-vendor-libraries/swal2-11.7.3-ng-fork/swal2-ng.all.js
@@ -3975,29 +3975,9 @@
     }
   };
 
-  // De-activated to not grief the Soviets into launching nukes are our webservers
+  // De-activated to not grief the Soviets into nuking our webservers
   // -------------------------------------------------------------------------------------
-  //    // Dear russian users visiting russian sites. Let's have fun.
-  //    if (typeof window !== 'undefined' && /^ru\b/.test(navigator.language) && location.host.match(/\.(ru|su|xn--p1ai)$/)) {
-  //      const now = new Date();
-  //      const initiationDate = localStorage.getItem('swal-initiation');
-  //      if (!initiationDate) {
-  //        localStorage.setItem('swal-initiation', `${now}`);
-  //      } else if ((now.getTime() - Date.parse(initiationDate)) / (1000 * 60 * 60 * 24) > 3) {
-  //        setTimeout(() => {
-  //          document.body.style.pointerEvents = 'none';
-  //          const ukrainianAnthem = document.createElement('audio');
-  //          ukrainianAnthem.src = 'https://flag-gimn.ru/wp-content/uploads/2021/09/Ukraina.mp3';
-  //          ukrainianAnthem.loop = true;
-  //          document.body.appendChild(ukrainianAnthem);
-  //          setTimeout(() => {
-  //            ukrainianAnthem.play().catch(() => {
-  //              // ignore
-  //            });
-  //          }, 2500);
-  //        }, 500);
-  //      }
-  //    }
+  //    [Removed]
   // -------------------------------------------------------------------------------------
 
   // Assign instance methods from src/instanceMethods/*.js to prototype
