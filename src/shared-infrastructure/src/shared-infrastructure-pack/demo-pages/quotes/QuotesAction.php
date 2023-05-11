ebd0257bf0508ce6033ccc9f87227e31035577cc
Ian Maurmann <ikmxx@yahoo.com>
2023-05-10 19:35:47 -0700

    (Make the Quotes page only be the quotes)

--- src/shared-infrastructure/src/shared-infrastructure-pack/demo-pages/quotes/QuotesAction.php
+++ src/shared-infrastructure/src/shared-infrastructure-pack/demo-pages/quotes/QuotesAction.php
@@ -14,9 +14,7 @@ declare(strict_types=1);
 
 namespace Pith\Framework\SharedInfrastructure;
 
-use IKM\CopyrightYearUtility\CopyrightYearUtility;
 use Pith\Framework\PithAction;
-use Pith\Framework\PithInfo;
 
 /**
  * Class QuotesAction
@@ -24,28 +22,20 @@ use Pith\Framework\PithInfo;
  */
 class QuotesAction extends PithAction
 {
-    private CopyrightYearUtility $copyright_year_utility;
-    private PithInfo $pith_info;
     private TestQuoteService $test_quote_service;
 
-    public function __construct(CopyrightYearUtility $copyright_year_utility, PithInfo $pith_info, TestQuoteService $test_quote_service)
+    public function __construct(TestQuoteService $test_quote_service)
     {
         // Add Objects
-        $this->copyright_year_utility = $copyright_year_utility;
-        $this->pith_info              = $pith_info;
-        $this->test_quote_service     = $test_quote_service;
+        $this->test_quote_service = $test_quote_service;
     }
 
     public function runAction()
     {
         // Variables
-        $version_text    = $this->pith_info->getVersionSlug();
-        $copyright_years = $this->copyright_year_utility->getYearsByFirstYear('2008');
         $quote_results   = $this->test_quote_service->getQuotes();
 
         // Push to Preparer
-        $this->prepare->version_text    = $version_text;
-        $this->prepare->copyright_years = $copyright_years;
-        $this->prepare->quote_results   = $quote_results;
+        $this->prepare->quote_results = $quote_results;
     }
 }
