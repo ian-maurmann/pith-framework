5e8812ac0e277fa56673d6545e4f3e749d17a745
Ian Maurmann <ikmxx@yahoo.com>
2023-05-12 15:31:14 -0700

    Workflow Element - Remove the Use for the App Reference Trait

--- src/framework/external/extend/PithWorkflowElement.php
+++ src/framework/external/extend/PithWorkflowElement.php
@@ -25,7 +25,6 @@ declare(strict_types=1);
 
 namespace Pith\Framework;
 
-use Pith\Framework\Internal\PithAppReferenceTrait;
 
 // ┌────────────────────────────────────────────────────────────────────────┐
 // │    PithWorkflowElement                                                 │
