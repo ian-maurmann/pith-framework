9f89f6fae26cce44a4ad0b0ac506caed45effb27
Ian Maurmann <ikmxx@yahoo.com>
2023-05-15 13:25:55 -0700

    Change the default view adapter to the Latte View Adapter

--- src/framework/external/extendable-workflow-elements/PithRoute.php
+++ src/framework/external/extendable-workflow-elements/PithRoute.php
@@ -156,7 +156,8 @@ class PithRoute extends PithWorkflowElement
      * Holds the namespace for the View Adapter object
      * @var string
      */
-    public string $view_adapter = '\\Pith\\PhtmlViewAdapter2\\PithPhtmlViewAdapter2'; // Use PithPhtmlViewAdapter2 as default
+ // public string $view_adapter = '\\Pith\\PhtmlViewAdapter2\\PithPhtmlViewAdapter2'; // Use PithPhtmlViewAdapter2 as default
+    public string $view_adapter = '\\Pith\\LatteViewAdapter\\PithLatteViewAdapter'; // Use the View Adapter for using Latte with Pith as the new default
 
     /**
      * Holds the namespace for the View Requisition object
