bcf66d0a8ab74ff9bfb531d9a9caa7aeed515f4a
Ian Maurmann <ikmxx@yahoo.com>
2023-05-15 22:56:54 -0700

    Add "Env Info" sidebar

---
+++ data/latte-cache/env-info-pages-sidebar-env-info-sidebar-view.latte--9b850f8588.php
@@ -0,0 +1,20 @@
+<?php
+
+use Latte\Runtime as LR;
+
+/** source: src/shared-infrastructure/src/shared-infrastructure-pack/env-info-pages/sidebar/env-info-sidebar-view.latte */
+final class Template9b850f8588 extends Latte\Runtime\Template
+{
+
+	public function main(array $ʟ_args): void
+	{
+		echo '<h4>Env Info Pages:</h4>
+<ul>
+    <li>
+        <a href="/11111/22222/env-info">Env Info</a>
+    </li>
+</ul>
+
+';
+	}
+}
