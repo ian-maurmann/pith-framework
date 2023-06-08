ac36d445d78202c5d96fbf3359c19067beb2edb9
Ian Maurmann <ikmxx@yahoo.com>
2023-06-08 10:36:40 -0700

    New User Signup - Show/hide password

--- src/shared-infrastructure/src/shared-ui-resource-pack/resources/application/user-system/new-user-signup.js
+++ src/shared-infrastructure/src/shared-ui-resource-pack/resources/application/user-system/new-user-signup.js
@@ -30,8 +30,12 @@ SharedUI.NewUserSignupForm.construct = function(){
 SharedUI.NewUserSignupForm.listen = function(){
     let self = SharedUI.NewUserSignupForm;
 
+    // Callout events
     Ox.Event.delegate('[data-shared-ui-focus-event="shared-ui.new-user-signup-form >>> on-field-focus"]', 'focus', self.handleOnFieldFocus);
     Ox.Event.delegate('[data-shared-ui-blur-event="shared-ui.new-user-signup-form >>> on-field-blur"]', 'blur', self.handleOnFieldBlur);
+
+    // Password show/hide events
+    Ox.Event.delegate('[data-shared-ui-click-event="shared-ui.new-user-signup-form >>> on-password-eye-click"]', 'click', self.handleOnPasswordEyeClick);
 }
 
 
@@ -56,6 +60,50 @@ SharedUI.NewUserSignupForm.handleOnFieldBlur = function(element, event){
 
     // Hide the callout
     callout.attr('data-show', 'no');
+}
+
+
+// Handle On Password-Eye Click
+SharedUI.NewUserSignupForm.handleOnPasswordEyeClick = function(element, event){
+    let self                   = SharedUI.NewUserSignupForm;
+    let eye                    = $(element);
+    let field                  = eye.parent().closest('[data-section-item-type="field"]');
+    let form                   = field.parent().closest('[data-section-item-type="form"]');
+    let show_form_passwords_yn = form.attr('data-form-show-passwords');
+    let show_form_passwords    = show_form_passwords_yn === 'yes';
+    let eyes                   = form.find('[data-section-item-role="password-eye"]');
+    let password_input         = form.find('[data-section-item="password-input"]').first();
+    let confirm_password_input = form.find('[data-section-item="confirm-password-input"]').first();
+
+    // Toggle Eyes
+    if(show_form_passwords){
+        // Hide passwords:
+
+        // Remove slashes from the eye icons
+        eyes.removeClass('bi-eye-slash');
+        eyes.addClass('bi-eye');
+
+        // Change the password input types
+        password_input.attr('type', 'password');
+        confirm_password_input.attr('type', 'password');
+
+        // Change the form's show-passwords value
+        form.attr('data-form-show-passwords', 'no');
+    }
+    else{
+        // Show passwords:
+
+        // Add slashes to the eye icons
+        eyes.removeClass('bi-eye');
+        eyes.addClass('bi-eye-slash');
+
+        // Change the password input types
+        password_input.attr('type', 'text');
+        confirm_password_input.attr('type', 'text');
+
+        // Change the form's show-passwords value
+        form.attr('data-form-show-passwords', 'yes');
+    }
 }
 
 
