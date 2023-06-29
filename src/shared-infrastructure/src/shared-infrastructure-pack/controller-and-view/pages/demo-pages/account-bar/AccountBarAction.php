39eef7775ec24d1d4f93fed3a48ddbf60a169102
Ian Maurmann <ikmxx@yahoo.com>
2023-06-29 15:11:37 -0700

    Use anti-CSRF token for logout link

--- src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/pages/demo-pages/account-bar/AccountBarAction.php
+++ src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/pages/demo-pages/account-bar/AccountBarAction.php
@@ -2,11 +2,12 @@
 
 /**
  * Account Bar Action
- * -------------
+ * ------------------
  *
  * @noinspection PhpPropertyNamingConventionInspection - Property names with underscores are ok.
  * @noinspection PhpMissingParentCallCommonInspection  - Action parent methods exist as fallback.
  * @noinspection PhpVariableNamingConventionInspection - Short variable names are ok.
+ * @noinspection PhpIllegalPsrClassPathInspection      - Using PSR-4 not PSR-0.
  */
 
 
@@ -14,11 +15,9 @@ declare(strict_types=1);
 
 namespace Pith\Framework\SharedInfrastructure;
 
-use IKM\CopyrightYearUtility\CopyrightYearUtility;
 use Pith\Framework\PithAction;
 use Pith\Framework\PithAppRetriever;
 use Pith\Framework\PithException;
-use Pith\Framework\PithInfo;
 
 /**
  * Class AccountBarAction
@@ -26,15 +25,11 @@ use Pith\Framework\PithInfo;
  */
 class AccountBarAction extends PithAction
 {
-    private CopyrightYearUtility $copyright_year_utility;
-    private PithInfo $pith_info;
     private PithAppRetriever $app_retriever;
 
-    public function __construct(CopyrightYearUtility $copyright_year_utility, PithInfo $pith_info, PithAppRetriever $app_retriever)
+    public function __construct(PithAppRetriever $app_retriever)
     {
-        // Add Objects
-        $this->copyright_year_utility = $copyright_year_utility;
-        $this->pith_info = $pith_info;
+        // Set object dependencies
         $this->app_retriever = $app_retriever;
     }
 
@@ -48,7 +43,7 @@ class AccountBarAction extends PithAction
 
         // Variables
         $is_logged_in    = $app->active_user->isLoggedIn();
-        $anti_csrf_token = $_SESSION['anti_csrf_token'] ?? '________';
+        $anti_csrf_token = $_SESSION['anti_csrf_token'] ?? '';
 
         // Push to Preparer
         $this->prepare->is_logged_in    = $is_logged_in;
