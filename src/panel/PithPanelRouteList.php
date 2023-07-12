7c43527c08f37bd2b8a54f74c5553702f12acaf5
Ian Maurmann <ikmxx@yahoo.com>
2023-07-12 14:56:04 -0700

    Getting started on Panel

---
+++ src/panel/PithPanelRouteList.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Pith Panel Route List
+ * ---------------------
+ *
+ * @noinspection PhpClassNamingConventionInspection - Long class names are ok.
+ */
+
+
+declare(strict_types=1);
+
+namespace Pith\Framework\Panel;
+
+use Pith\Framework\PithRouteList;
+
+/**
+ * Class PithPanelRouteList
+ * @package Pith\Framework\Panel
+ */
+class PithPanelRouteList extends PithRouteList
+{
+    public array $routes = [
+        ['route', 'GET', '/resources/{filepath:.+}', '\\Pith\\Framework\\Panel\\Theme\\ThemeResourcesRoute'],
+    ];
+}
