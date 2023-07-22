b28ac63ed48ae070dfe61cfcbf6c3b1aa0b4e63c
Ian Maurmann <ikmxx@yahoo.com>
2023-07-21 17:15:16 -0700

    (Rename QueueImpressionLogForImport to QueueImpressionLogsForImport)

--- src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/tasks/TasksRouteList.php
+++ src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/tasks/TasksRouteList.php
@@ -24,6 +24,6 @@ class TasksRouteList extends PithRouteList
 {
     public array $routes = [
         ['route', ['GET'], '/import_impression_log_to_database', '\\Pith\\Framework\\SharedInfrastructure\\Tasks\\ImpressionLoggingTasks\\ImportImpressionLogToDatabaseTaskRoute'],
-        ['route', ['GET'], '/queue_impression_log_for_import',   '\\Pith\\Framework\\SharedInfrastructure\\Tasks\\ImpressionLoggingTasks\\QueueImpressionLogForImportTaskRoute'],
+        ['route', ['GET'], '/queue_impression_logs_for_import',  '\\Pith\\Framework\\SharedInfrastructure\\Tasks\\ImpressionLoggingTasks\\QueueImpressionLogsForImportTaskRoute'],
     ];
 }
--- src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/tasks/impression-logging-tasks/queue-impression-logs-for-import/QueueImpressionLogsForImportTaskRoute.php
+++ src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/tasks/impression-logging-tasks/queue-impression-logs-for-import/QueueImpressionLogsForImportTaskRoute.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Queue Impression Log For Import task route
+ * Queue Impression Logs For Import task route
  * ------------------------------------------
  *
  * @noinspection PhpPropertyNamingConventionInspection - Ignore.
@@ -17,14 +17,14 @@ namespace Pith\Framework\SharedInfrastructure\Tasks\ImpressionLoggingTasks;
 use Pith\Framework\PithRoute;
 
 /**
- * Class QueueImpressionLogForImportTaskRoute
+ * Class QueueImpressionLogsForImportTaskRoute
  * @package Pith\Framework\SharedInfrastructure\Tasks\ImpressionLoggingTasks
  */
-class QueueImpressionLogForImportTaskRoute extends PithRoute
+class QueueImpressionLogsForImportTaskRoute extends PithRoute
 {
     public string $route_type   = 'task';
     public string $pack         = '\\Pith\\Framework\\SharedInfrastructure\\SharedInfrastructurePack';
     public string $access_level = 'world';
-    public string $action       = '\\Pith\\Framework\\SharedInfrastructure\\Tasks\\ImpressionLoggingTasks\\QueueImpressionLogForImportTaskAction';
+    public string $action       = '\\Pith\\Framework\\SharedInfrastructure\\Tasks\\ImpressionLoggingTasks\\QueueImpressionLogsForImportTaskAction';
     public string $view_adapter = '\\Pith\\CliViewAdapter\\PithCliViewAdapter';
 }
