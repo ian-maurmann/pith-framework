705cce4f2bec92320aa528fd53242fff806677fc
Ian Maurmann <ikmxx@yahoo.com>
2023-08-05 14:58:12 -0700

    (Migration change)

--- data/migrations/Version20230805204936.php
+++ data/migrations/Version20230805204936.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Migration to add `unique_impressions` table
- * ------------------------------------
+ * Migration to add `unique_daily_views` table
+ * -------------------------------------------
  *
  * @noinspection PhpClassNamingConventionInspection   - Long class name is ok.
  * @noinspection PhpMissingParentCallCommonInspection - Parent method calls are not needed.
@@ -24,20 +24,23 @@ final class Version20230805204936 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Create new table `unique_impressions`.';
+        return 'Create new table `unique_daily_views`.';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('
-            CREATE TABLE `unique_impressions` (
-                `unique_impression_id` BIGINT AUTO_INCREMENT UNIQUE NOT NULL, 
+            CREATE TABLE `unique_daily_views` (
+                `unique_daily_view_id` BIGINT AUTO_INCREMENT UNIQUE NOT NULL, 
+                `unique_weekly_view_id` BIGINT DEFAULT NULL, 
+                `unique_monthly_view_id` BIGINT DEFAULT NULL, 
+                `unique_actor_id` BIGINT DEFAULT NULL, 
                 `date` DATETIME NOT NULL,
                 `ip` VARCHAR(191) NOT NULL,
                 `user_id` INT NOT NULL,
                 `user_agent_string` VARCHAR(191) NOT NULL,
-                PRIMARY KEY (`unique_impression_id`),
-                UNIQUE KEY `unique_impressions_uk_all` (`date`,`ip`, `user_id`, `user_agent_string`)
+                PRIMARY KEY (`unique_daily_view_id`),
+                UNIQUE KEY `unique_daily_views_uk_identifiers` (`date`,`ip`, `user_id`, `user_agent_string`)
             )
             ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
             '
@@ -46,6 +49,6 @@ final class Version20230805204936 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE IF EXISTS `uniques`');
+        $this->addSql('DROP TABLE IF EXISTS `unique_daily_views`');
     }
 }
