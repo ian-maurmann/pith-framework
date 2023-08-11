75592442aba0e21ec4fdeb15bdaf0a1f51591eb6
Ian Maurmann <ikmxx@yahoo.com>
2023-08-10 17:00:27 -0700

    (Testing the Scheduler)

--- scheduler.php
+++ scheduler.php
@@ -1,11 +1,20 @@
-<?php require_once __DIR__.'/vendor/autoload.php';
+<?php
+// scheduler.php
+// See https://github.com/peppeocchi/php-cron-scheduler
+
+
+require_once __DIR__.'/vendor/autoload.php';
 
 use GO\Scheduler;
+
 
 // Create a new scheduler
 $scheduler = new Scheduler();
 
-// ... configure the scheduled jobs (see below) ...
+
+// ... configure the scheduled jobs ...
+$scheduler->raw('touch scheduler_was_run.txt')->everyMinute();
+
 
 // Let the scheduler execute jobs which are due.
 $scheduler->run();
