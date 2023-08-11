38abb069fcb797b8b18fbde94701183b6826027d
Ian Maurmann <ikmxx@yahoo.com>
2023-08-10 18:24:55 -0700

    Schedule impression log tasks

--- scheduler.php
+++ scheduler.php
@@ -26,7 +26,17 @@ $scheduler = new Scheduler();
 
 
 // ... configure the scheduled jobs ...
-$scheduler->raw('touch scheduler_was_run.txt')->everyMinute();
+//$scheduler->raw('touch scheduler_was_run.txt')->everyMinute();
+//$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/hello_task"')->everyMinute();
+//$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/hello_world"')->everyMinute();
+//$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/tick"')->everyMinute();
+
+// Impression Logs
+$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/queue_impression_logs_for_import"')->hourly(5);
+$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/import_impression_log_to_database"')->everyMinute(7);
+$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/delete_loaded_impression_log"')->everyMinute(9);
+$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/cleanup_impression_log_loading_queue"')->hourly(2);
+$scheduler->raw('curl --request GET "http://127.0.0.1:8080/shared-infrastructure/run/task/gather_unique_daily_views"')->hourly(22);
 
 
 // Let the scheduler execute jobs which are due.
