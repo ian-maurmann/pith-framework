1490f0f25f09c6a9bb87f8bb0c29fd063fe4eab8
Ian Maurmann <ikmxx@yahoo.com>
2023-11-20 18:51:36 -0700

    Add the app Route List namespace to the Tracked Constants file

--- tracked-constants.php
+++ tracked-constants.php
@@ -18,6 +18,9 @@ if(!defined('STDOUT')){
 }
 
 // Define our Constants
+
+const PITH_APP_ROUTE_LIST = '\\Pith\\Framework\\SharedInfrastructure\\SharedInfrastructureRouteList';
+
 const PITH_DEMO_PAGE_MAIN_TITLE            = 'Demo Page - Pith Framework';
 const PITH_DEMO_PAGES_ROUTE_GROUP_PATH     = '/1111/1111/demo';
 const PITH_ENV_INFO_PAGES_ROUTE_GROUP_PATH = '/2222/2222/env-info';
