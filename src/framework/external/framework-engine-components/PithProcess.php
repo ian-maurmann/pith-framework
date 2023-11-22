62876077c2e54195ce1e4350b749dd87b02d09df
Ian Maurmann <ikmxx@yahoo.com>
2023-11-21 20:20:19 -0700

    Add Process object

---
+++ src/framework/external/framework-engine-components/PithProcess.php
@@ -0,0 +1,31 @@
+<?php
+# ===================================================================
+# Copyright (c) 2008-2023 Ian K Maurmann. The Pith Framework is
+# provided under the terms of the Mozilla Public License, v. 2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ===================================================================
+
+
+/**
+ * Pith Process
+ * ------------
+ */
+
+
+declare(strict_types=1);
+
+
+namespace Pith\Framework;
+
+
+/**
+ * Class PithProcess
+ * @package Pith\Framework
+ */
+class PithProcess
+{
+    public string $process_type = 'unknown';
+}
