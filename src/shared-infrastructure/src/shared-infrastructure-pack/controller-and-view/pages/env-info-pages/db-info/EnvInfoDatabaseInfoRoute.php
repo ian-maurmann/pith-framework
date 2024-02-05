5c49494b3948f13980ad73efad07cae35a39ff2d
Ian Maurmann <ikmxx@yahoo.com>
2024-02-04 19:36:00 -0700

    Update routes to use workflow

--- src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/pages/env-info-pages/db-info/EnvInfoDatabaseInfoRoute.php
+++ src/shared-infrastructure/src/shared-infrastructure-pack/controller-and-view/pages/env-info-pages/db-info/EnvInfoDatabaseInfoRoute.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace Pith\Framework\SharedInfrastructure\Pages\EnvInfoPages;
 
-//use Pith\Framework\PithRoute;
+//use Pith\Workflow\PithRoute;
 use Pith\Workflow\PithRoute;
 
 /**
--- src/shared-infrastructure/src/shared-theme-pack/env-info-theme/EnvInfoLayoutRoute.php
+++ src/shared-infrastructure/src/shared-theme-pack/env-info-theme/EnvInfoLayoutRoute.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace Pith\Framework\SharedThemePack;
 
-//use Pith\Framework\PithRoute;
+//use Pith\Workflow\PithRoute;
 use Pith\Workflow\PithRoute;
 
 /**
