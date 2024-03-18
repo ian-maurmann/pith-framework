13ab0902a912c19e9578787157963b7cc0784064
Ian Maurmann <ikmxx@yahoo.com>
2024-03-17 23:49:47 -0700

    (Remove hyphen)

--- src/panel/packs/panel-theme-pack/main-layout/MainLayoutViewRequisition.php
+++ src/panel/packs/panel-theme-pack/main-layout/MainLayoutViewRequisition.php
@@ -45,15 +45,15 @@ class MainLayoutViewRequisition extends PithViewRequisition
         $this->addStylesheet('Font Awesome 4 compatibility', '/resources/vendor/library/font-awesome/font-awesome-4.7.0-compatibility-fork/css/font-awesome-4-compatibility-fork.min.css', 'library-for-layout');
         $this->addStylesheet('Bootstrap Icons stylesheet', '/resources/vendor/library/bootstrap/bootstrap-icons-1.10.5/bootstrap-icons.min.css', 'library-for-layout');
         $this->addStylesheet('OxCSS stylesheet',  '/resources/vendor/library/oxcss/oxcss-0.1.4/dist/ox.css', 'library-for-layout');
-        $this->addStylesheet('Swal2 NA stylesheet', '/resources/vendor/library/sweet-alert/swal2-no-anthems-11.7.3/swal2-na.css', 'library-for-layout');
-        $this->addStylesheet('Re-style Swal2', '/resources/vendor/library/sweet-alert/swal2-custom-themes/swal2-aerogel-theme.css', 'library-for-layout');
+        $this->addStylesheet('Swal2 NA stylesheet', '/resources/vendor/library/sweetalert/swal2-no-anthems-11.7.3/swal2-na.css', 'library-for-layout');
+        $this->addStylesheet('Re-style Swal2', '/resources/vendor/library/sweetalert/swal2-custom-themes/swal2-aerogel-theme.css', 'library-for-layout');
         $this->addStylesheet('Hoja Aquamarine stylesheet', '/resources/vendor/library/hoja/hoja-aquamarine/hoja-aquamarine.css', 'library-for-layout');
         $this->addStylesheet('Aero-Gel stylesheet', '/resources/vendor/library/aero-gel/aero-gel-1.0.0/aero-gel.css', 'library-for-layout');
 
         // JS Libraries for layout
         $this->addScript('jQuery', '/resources/vendor/library/jquery/jquery-3.6.4/jquery-3.6.4.min.js', 'library-for-layout');
         $this->addScript('OxCSS script','/resources/vendor/library/oxcss/oxcss-0.1.4/dist/ox.js', 'library-for-layout');
-        $this->addScript('Swal2 NA script', '/resources/vendor/library/sweet-alert/swal2-no-anthems-11.7.3/swal2-na.all.js', 'library-for-layout');
+        $this->addScript('Swal2 NA script', '/resources/vendor/library/sweetalert/swal2-no-anthems-11.7.3/swal2-na.all.js', 'library-for-layout');
 
         // App CSS for layout
         $this->addStylesheet('Panel Stylesheet', PITH_PANEL_PATH . '/resources/panel-theme.css', 'application-for-layout');
