fb0e3e357c5b059699c48ded10df14b44af1ed98
Ian Maurmann <ikmxx@yahoo.com>
2024-09-28 19:33:10 -0700

    Fix unit test for testing access level

--- tests/Unit/Framework/Access-Level-Workflow-Element/PithAccessLevelTest.php
+++ tests/Unit/Framework/Access-Level-Workflow-Element/PithAccessLevelTest.php
@@ -1,14 +1,25 @@
 <?php
 
+
+/**
+ * Access Level Test
+ * -----------------
+ *
+ * @noinspection PhpPropertyNamingConventionInspection - Property names with underscores are ok.
+ * @noinspection PhpVariableNamingConventionInspection - Short variable names are ok.
+ */
+
+declare(strict_types=1);
+
 test('Access Level :: getName( ) is string.', function () {
-    $base_access_level = new Pith\Framework\PithAccessLevel();
+    $base_access_level = new Pith\Workflow\PithAccessLevel();
     $name = $base_access_level->getName();
 
     expect($name)->toBeString();
 });
 
 test('Access Level :: isAllowedToAccess( ) is bool.', function () {
-    $base_access_level = new Pith\Framework\PithAccessLevel();
+    $base_access_level = new Pith\Workflow\PithAccessLevel();
     $is_allowed_to_access = $base_access_level->isAllowedToAccess();
 
     expect($is_allowed_to_access)->toBeBool();
