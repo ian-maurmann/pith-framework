b354076846ec5cf993db330b80c527977cb44167
Ian Maurmann <ikmxx@yahoo.com>
2025-02-18 22:29:52 -0700

    Fix namespacing after moving the Grouping Utility

--- src/plugins/grouping-utility/src/GroupingUtility.php
+++ src/plugins/grouping-utility/src/GroupingUtility.php
@@ -24,12 +24,11 @@
 declare(strict_types=1);
 
 
-namespace Pith\Framework\SharedInfrastructure\Model\Format;
+namespace Pith\Framework\Plugin\GroupingUtility;
 
 
 /**
  * Class GroupingUtility
- * @package Pith\Framework\SharedInfrastructure\Model\Random
  */
 class GroupingUtility
 {
