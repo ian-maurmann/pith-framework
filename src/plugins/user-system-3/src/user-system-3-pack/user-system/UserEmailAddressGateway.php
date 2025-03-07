490eac1f393fe7f72c27dc57d88cd4e33afb5380
Ian Maurmann <ikmxx@yahoo.com>
2025-03-06 20:56:06 -0700

    Put user system model into user system plugin

---
+++ src/plugins/user-system-3/src/user-system-3-pack/user-system/UserEmailAddressGateway.php
@@ -0,0 +1,81 @@
+<?php
+# ===================================================================
+# Copyright (c) 2008-2025 Ian K Maurmann. The Pith Framework is
+# provided under the terms of the Mozilla Public License, v. 2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ===================================================================
+
+
+/**
+ * User Email Address Gateway
+ * --------------------------
+ *
+ * @noinspection PhpClassNamingConventionInspection    - Long class name is ok.
+ * @noinspection PhpVariableNamingConventionInspection - Short variable name are ok.
+ * @noinspection PhpMethodNamingConventionInspection   - Long method names are ok.
+ * @noinspection PhpIllegalPsrClassPathInspection      - Ignore, using PSR 4 not 0.
+ */
+
+
+declare(strict_types=1);
+
+
+namespace Pith\Framework\Plugin\UserSystem3;
+
+use Exception;
+use PDOException;
+use Pith\Framework\PithDatabaseWrapper;
+use Pith\Framework\PithException;
+
+/**
+ * Class UserEmailAddressGateway
+ */
+class UserEmailAddressGateway
+{
+    private PithDatabaseWrapper $database;
+
+    public function __construct(PithDatabaseWrapper $database)
+    {
+        $this->database = $database;
+    }
+
+    /**
+     * @param int $user_id
+     * @param string $email_address
+     * @return int
+     * @throws Exception
+     */
+    public function addUserEmailAddress(int $user_id, string $email_address): int
+    {
+        // Query
+        $sql = '
+            INSERT INTO `user_email_addresses` 
+                (user_id, email_address) 
+            VALUES 
+                (:user_id, :email_address) 
+            ';
+
+        // Prepare
+        $statement = $this->database->pdo->prepare($sql);
+
+        // Execute
+        $statement->execute(
+            [
+                ':user_id'       => $user_id,
+                ':email_address' => $email_address,
+            ]
+        );
+
+        // Get inserted id
+        $inserted_id = $this->database->pdo->lastInsertId() ?: 0;
+        if($inserted_id === 0){
+            throw new Exception('Failed to insert to the User table.');
+        }
+
+        // Return the inserted id
+        return (int) $inserted_id;
+    }
+}
