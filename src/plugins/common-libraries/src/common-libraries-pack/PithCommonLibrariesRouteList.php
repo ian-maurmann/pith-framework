e9e01778c3a6d40715342ee9876cc518c6064076
Ian Maurmann <ikmxx@yahoo.com>
2025-04-20 20:38:31 -0700

    Add more paths to common libraries path

--- src/plugins/common-libraries/src/common-libraries-pack/PithCommonLibrariesRouteList.php
+++ src/plugins/common-libraries/src/common-libraries-pack/PithCommonLibrariesRouteList.php
@@ -21,8 +21,20 @@ use Pith\Workflow\PithRouteList;
 class PithCommonLibrariesRouteList extends PithRouteList
 {
     public array $routes = [
-        ['route', 'GET', '/aero-gel/{filepath:.+}',    '\\PithFront\\PithPackAeroGel\\AeroGelResourceRoute'],
-        ['route', 'GET', '/animate.css/{filepath:.+}', '\\PithFront\\PithPackAnimateCss\\AnimateCssResourceRoute'],
-        ['route', 'GET', '/bootstrap/{filepath:.+}',   '\\PithFront\\PithPackBootstrap\\BootstrapResourceRoute'],
+        ['route', 'GET', '/aero-gel/{filepath:.+}',          '\\PithFront\\PithPackAeroGel\\AeroGelResourceRoute'],
+        ['route', 'GET', '/animate.css/{filepath:.+}',       '\\PithFront\\PithPackAnimateCss\\AnimateCssResourceRoute'],
+        ['route', 'GET', '/bootstrap/{filepath:.+}',         '\\PithFront\\PithPackBootstrap\\BootstrapResourceRoute'],
+        ['route', 'GET', '/font-awesome/{filepath:.+}',      '\\PithFront\\PithPackFaIcons\\FaIconsResourceRoute'],
+        ['route', 'GET', '/fixie-reset/{filepath:.+}',       '\\PithFront\\PithPackFixie\\FixieResourceRoute'],
+        ['route', 'GET', '/hoja/{filepath:.+}',              '\\PithFront\\PithPackHojaRing\\HojaRingResourceRoute'],
+        ['route', 'GET', '/ibm-plex/{filepath:.+}',          '\\PithFront\\PithPackPlex\\PlexResourceRoute'],
+        ['route', 'GET', '/jetbrains-mono-nl/{filepath:.+}', '\\PithFront\\PithPackJbMonoNl\\JbMonoNlResourceRoute'],
+        ['route', 'GET', '/jquery/{filepath:.+}',            '\\PithFront\\PithPackJquery\\JqueryResourceRoute'],
+        ['route', 'GET', '/jscrollpane/{filepath:.+}',       '\\PithFront\\PithPackJscrollpane\\JscrollpaneResourceRoute'],
+        ['route', 'GET', '/md-icons/{filepath:.+}',          '\\PithFront\\PithPackMdIcons\\MdIconsResourceRoute'],
+        ['route', 'GET', '/oxcss/{filepath:.+}',             '\\PithFront\\PithPackOxcss\\OxcssResourceRoute'],
+        ['route', 'GET', '/src-fallback/{filepath:.+}',      '\\PithFront\\PithPackSrcFallback\\SrcFallbackResourceRoute'],
+        ['route', 'GET', '/sweetalert/{filepath:.+}',        '\\PithFront\\PithPackSwal\\SwalResourceRoute'],
+        ['route', 'GET', '/toastr/{filepath:.+}',            '\\PithFront\\PithPackToastr\\ToastrResourceRoute'],
     ];
 }
