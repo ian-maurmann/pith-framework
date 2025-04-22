a79d8845276bc395651dfac7ec2b0150044f0a2f
Ian Maurmann <ikmxx@yahoo.com>
2025-04-21 22:46:20 -0700

    (Also load stylesheet from CDN)

--- src/plugins/shared-infrastructure-DEPRECATED/src/shared-infrastructure-pack/controller-and-view/pages/demo-pages/demonstrate-libraries-work/DemonstrateLoadingFromCdnViewRequisition.php
+++ src/plugins/shared-infrastructure-DEPRECATED/src/shared-infrastructure-pack/controller-and-view/pages/demo-pages/demonstrate-libraries-work/DemonstrateLoadingFromCdnViewRequisition.php
@@ -34,14 +34,14 @@ class DemonstrateLoadingFromCdnViewRequisition extends PithViewRequisition
         //     5 - application-for-page
         //     6 - application-for-partial
 
-        // Resources
-        $this->addStylesheet('OxCSS stylesheet', '/resources/vendor/common-libraries/oxcss/oxcss-0.1.4/dist/ox.css', 'library-for-page');
+        // Resources:
 
+        // Animate.css Stylesheet
+        // $this->addStylesheet('Animate.css Stylesheet', '/resources/vendor/common-libraries/animate.css/animate.css-4.1.1/animate.min.css', 'library-for-page');
+        $this->addCdnStylesheet('Animate.css Stylesheet', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', 'sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==', 'anonymous', '/resources/vendor/common-libraries/animate.css/animate.css-4.1.1/animate.min.css', 'library-for-page');
 
+        // jQuery Script
         //$this->addScript('jquery', '/resources/vendor/common-libraries/jquery/jquery-3.6.4/jquery-3.6.4.min.js', 'library-for-page');
-
         $this->addCdnScript('jQuery', 'https://code.jquery.com/jquery-3.7.1.min.js', 'sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=', 'anonymous', '/resources/vendor/common-libraries/jquery/jquery-3.6.4/jquery-3.6.4.min.js', 'library-for-page');
-
-        $this->addScript('OxCSS script', '/resources/vendor/common-libraries/oxcss/oxcss-0.1.4/dist/ox.js', 'library-for-page');
     }
 }
