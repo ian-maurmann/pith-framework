f398f44b7b7fb15ee6af149d9c923850b8a0b70c
Ian Maurmann <ikmxx@yahoo.com>
2025-05-02 22:50:31 -0700

    Setup - Add prompt for project namespace

--- src/plugins/command-tool-2/src/cli/PithSetup.php
+++ src/plugins/command-tool-2/src/cli/PithSetup.php
@@ -29,70 +29,114 @@ class PithSetup
 
     public function run()
     {
-        $output = "\n" . 'Setting up new Pith Framework project.'. "\n";
+        $format = new CommandLineFormatter();
+
+        $is_ready = true;
+
+        // VendorNamespace\ProjectNamespace
+
+        fwrite(STDOUT, "\n");
+        $output = 'Add a namespace to use for this project.' . "\n"
+            . "\n"
+            . '████ The normal format to use: ' . $format->bg_dark_black . $format->fg_dark_yellow . $format->italic . '{vendor namespace here} ' . $format->reset . $format->bg_dark_black . $format->fg_bright_yellow . '\\' . $format->fg_dark_yellow . $format->italic . ' {project namespace here}' . $format->reset . "\n";
         fwrite(STDOUT, $output);
 
-        // Cache
-        $this->existFolder('./cache');
-        $this->existFolder('./cache/latte-cache');
-        $this->existMdFile('./cache/latte-cache/latte-cache.md', 'Latte will put the Latte cache files in this folder.');
-        $this->existFolder('./cache/touchstones');
-        $this->existMdFile('./cache/touchstones/about-touchstone-folder.md', 'The Touchstone Utility will put empty files in this folder.');
-        $this->existFolder('./cache/touchstones/impression-system');
-        $this->existMdFile('./cache/touchstones/impression-system/about-impression-touchstone-folder.md', 'The impression system will put empty files in this folder.');
-        $this->existFolder('./cache/touchstones/janitor');
-        $this->existMdFile('./cache/touchstones/janitor/about-janitor-touchstone-folder.md', 'The Janitor system will put empty files in this folder.');
+        $output = '████ Example 1: ' . $format->bg_dark_black . $format->fg_bright_yellow . 'ExampleCompanyStudios\\OurAwesomeWebProject' . $format->reset . "\n";
+        fwrite(STDOUT, $output);
 
-        // Logs
-        $this->existFolder('./logs');
-        $this->existFolder('./logs/impression-logs');
-        $this->existMdFile('./logs/impression-logs/about-impression-logs.md', 'The impression logs will go here.');
-        $this->existFolder('./logs/php-error-logs');
-        $this->existMdFile('./logs/php-error-logs/about-php-error-logs.md', 'The PHP error log files will go in this folder.');
-        $this->existFolder('./logs/task-logs');
-        $this->existMdFile('./logs/task-logs/about-task-logs.md', 'The task logs will go in this folder.');
-        $this->existFolder('./logs/task-output-logs');
-        $this->existMdFile('./logs/task-output-logs/about-task-output-logs.md', 'The task output logs will go in this folder.');
+        $output = '████ Example 2: ' . $format->bg_dark_black . $format->fg_bright_yellow . 'JohnDoe\\MyCoolProject' . $format->reset . "\n";
+        fwrite(STDOUT, $output);
 
-        // Migrations
-        $this->existFolder('./migrations');
-        $this->existMdFile('./migrations/about-migrations.md', 'Migrations will go here.');
 
-        // Run
-        $this->existFolder('./run');
-        $this->existFolder('./run/public-local');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/about-how-to-run-on-local.md', './run/public-local/about-how-to-run-on-local.md');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/favicon.png', './run/public-local/favicon.png');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/index.php', './run/public-local/index.php');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/serve.php', './run/public-local/serve.php');
-        $this->existFolder('./run/public-web');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/.htaccess', './run/public-web/.htaccess');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/about-how-to-run-on-web-host.md', './run/public-web/about-how-to-run-on-web-host.md');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/favicon.png', './run/public-web/favicon.png');
-        $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/index.php', './run/public-web/index.php');
+        $project_full_namespace = '';
+        if($is_ready){
+            fwrite(STDOUT, "\n");
+            $output = $format->reset . 'Project Namespace: ';
+            do{
+                $input = readline($output);
+            } while(empty($input));
+        }
+        $project_full_namespace = $input;
+        fwrite(STDOUT, $format->reset . "\n");
 
-        // Src
-        $this->existFolder('./src');
+        // Ask to run set up
+        $setup_new_pith_project = false;
+        if($is_ready){
+            fwrite(STDOUT, "\n");
+            $output = 'Create Pith project? (yes/no): ';
 
-        // Git
-        $this->copyFileIfNotExists('./vendor/pith/framework/.gitignore', './.gitignore');
+            do{
+                $input = readline($output);
+            } while(empty($input));
+        }
+        $setup_new_pith_project = $input === 'yes';
 
-        // Env
-        $this->copyFileIfNotExists('./vendor/pith/framework/env.dist.php', './env.dist.php');
-        $this->copyFileIfNotExists('./vendor/pith/framework/env.dist.php', './env.php');
+        if($setup_new_pith_project) {
+            $output = "\n" . 'Setting up new Pith Framework project.'. "\n";
+            fwrite(STDOUT, $output);
 
-        // Front Controller
-        $this->copyFileIfNotExists('./vendor/pith/framework/front-controller.php', './front-controller.php');
+            // Cache
+            $this->existFolder('./cache');
+            $this->existFolder('./cache/latte-cache');
+            $this->existMdFile('./cache/latte-cache/latte-cache.md', 'Latte will put the Latte cache files in this folder.');
+            $this->existFolder('./cache/touchstones');
+            $this->existMdFile('./cache/touchstones/about-touchstone-folder.md', 'The Touchstone Utility will put empty files in this folder.');
+            $this->existFolder('./cache/touchstones/impression-system');
+            $this->existMdFile('./cache/touchstones/impression-system/about-impression-touchstone-folder.md', 'The impression system will put empty files in this folder.');
+            $this->existFolder('./cache/touchstones/janitor');
+            $this->existMdFile('./cache/touchstones/janitor/about-janitor-touchstone-folder.md', 'The Janitor system will put empty files in this folder.');
 
-        // Migrations tool
-        $this->copyFileIfNotExists('./vendor/pith/framework/mig', './mig');
-        $this->copyFileIfNotExists('./vendor/pith/framework/migration-config.php', './migration-config.php');
+            // Logs
+            $this->existFolder('./logs');
+            $this->existFolder('./logs/impression-logs');
+            $this->existMdFile('./logs/impression-logs/about-impression-logs.md', 'The impression logs will go here.');
+            $this->existFolder('./logs/php-error-logs');
+            $this->existMdFile('./logs/php-error-logs/about-php-error-logs.md', 'The PHP error log files will go in this folder.');
+            $this->existFolder('./logs/task-logs');
+            $this->existMdFile('./logs/task-logs/about-task-logs.md', 'The task logs will go in this folder.');
+            $this->existFolder('./logs/task-output-logs');
+            $this->existMdFile('./logs/task-output-logs/about-task-output-logs.md', 'The task output logs will go in this folder.');
 
-        // Pith command tool
-        $this->copyFileIfNotExists('./vendor/pith/framework/pith', './pith');
+            // Migrations
+            $this->existFolder('./migrations');
+            $this->existMdFile('./migrations/about-migrations.md', 'Migrations will go here.');
 
-        // Task tool
-        $this->copyFileIfNotExists('./vendor/pith/framework/task', './task');
+            // Run
+            $this->existFolder('./run');
+            $this->existFolder('./run/public-local');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/about-how-to-run-on-local.md', './run/public-local/about-how-to-run-on-local.md');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/favicon.png', './run/public-local/favicon.png');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/index.php', './run/public-local/index.php');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-local/serve.php', './run/public-local/serve.php');
+            $this->existFolder('./run/public-web');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/.htaccess', './run/public-web/.htaccess');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/about-how-to-run-on-web-host.md', './run/public-web/about-how-to-run-on-web-host.md');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/favicon.png', './run/public-web/favicon.png');
+            $this->copyFileIfNotExists('./vendor/pith/framework/run/public-web/index.php', './run/public-web/index.php');
+
+            // Src
+            $this->existFolder('./src');
+
+            // Git
+            $this->copyFileIfNotExists('./vendor/pith/framework/.gitignore', './.gitignore');
+
+            // Env
+            $this->copyFileIfNotExists('./vendor/pith/framework/env.dist.php', './env.dist.php');
+            $this->copyFileIfNotExists('./vendor/pith/framework/env.dist.php', './env.php');
+
+            // Front Controller
+            $this->copyFileIfNotExists('./vendor/pith/framework/front-controller.php', './front-controller.php');
+
+            // Migrations tool
+            $this->copyFileIfNotExists('./vendor/pith/framework/mig', './mig');
+            $this->copyFileIfNotExists('./vendor/pith/framework/migration-config.php', './migration-config.php');
+
+            // Pith command tool
+            $this->copyFileIfNotExists('./vendor/pith/framework/pith', './pith');
+
+            // Task tool
+            $this->copyFileIfNotExists('./vendor/pith/framework/task', './task');
+        }
     }
 
     public function existFolder(string $folder_path){
