41a573726eab6db5d48b113697594fc4cf9beee9
Ian Maurmann <ikmxx@yahoo.com>
2025-05-13 20:48:06 -0700

    Rename const for login page url

--- src/framework/external/framework-engine-components/PithActiveUser.php
+++ src/framework/external/framework-engine-components/PithActiveUser.php
@@ -225,7 +225,7 @@ class PithActiveUser
         }
         else{
             // Redirect to user failed login form
-            header('Location: ' . SHARED_UI_USER_LOGIN_FORM_PAGE_LINK . '?login-failed', true, 302);
+            header('Location: ' . PITH_APP_DEFAULT_LOGIN_PAGE_URL_PATH . '?login-failed', true, 302);
             exit;
         }
     }
