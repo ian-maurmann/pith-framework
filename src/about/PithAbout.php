176b322fefd7e11432265acf24eb4fa9ca414277
Ian Maurmann <ikmxx@yahoo.com>
2025-06-02 20:15:44 -0700

    Update version numbers

--- src/about/PithAbout.php
+++ src/about/PithAbout.php
@@ -32,7 +32,7 @@ class PithAbout
     public string $copyright      = 'Copyright (c) 2008-2025 Ian K Maurmann';
     public string $license        = 'Mozilla Public License, v. 2.0';
     public string $release_status = 'Alpha';
-    public string $release_name   = 'Alpha 56';
-    public string $real_version   = '0.9.0.5';
-    public string $semver_version = '0.40.0';
+    public string $release_name   = 'Alpha 57';
+    public string $real_version   = '0.9.1.0';
+    public string $semver_version = '0.41.0';
 }
