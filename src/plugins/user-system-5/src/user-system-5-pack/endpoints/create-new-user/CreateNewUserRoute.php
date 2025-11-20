62ffa33c6059c1529c663178919836005ce20d86
Ian Maurmann <ikmxx@yahoo.com>
2025-11-19 23:31:47 -0700

    Update namespacing for endpoint to create new user

--- src/plugins/user-system-5/src/user-system-5-pack/endpoints/create-new-user/CreateNewUserRoute.php
+++ src/plugins/user-system-5/src/user-system-5-pack/endpoints/create-new-user/CreateNewUserRoute.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Pith\Framework\Plugin\UserSystem4;
+namespace Pith\Framework\Plugin\UserSystem5;
 
 use Pith\Workflow\PithRoute;
 
@@ -21,8 +21,8 @@ use Pith\Workflow\PithRoute;
 class CreateNewUserRoute extends PithRoute
 {
     public string $route_type   = 'endpoint';
-    public string $pack         = '\\Pith\\Framework\\Plugin\\UserSystem4\\PithUserSystem4Pack';
+    public string $pack         = '\\Pith\\Framework\\Plugin\\UserSystem5\\PithUserSystem5Pack';
     public string $access_level = 'world';
-    public string $action       = '\\Pith\\Framework\\Plugin\\UserSystem4\\CreateNewUserAction';
+    public string $action       = '\\Pith\\Framework\\Plugin\\UserSystem5\\CreateNewUserAction';
     public string $view_adapter = '\\Pith\\JsonEndpointViewAdapter\\PithJsonEndpointViewAdapter';
 }
