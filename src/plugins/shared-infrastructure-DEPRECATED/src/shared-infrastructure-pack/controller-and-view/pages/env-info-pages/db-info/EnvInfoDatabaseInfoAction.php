3b21cdda9db492fb153b9558af942565ba5ad274
Ian Maurmann <ikmxx@yahoo.com>
2026-08-15 21:59:22 -0700

    Add new database vars to the env info page

--- src/plugins/shared-infrastructure-DEPRECATED/src/shared-infrastructure-pack/controller-and-view/pages/env-info-pages/db-info/EnvInfoDatabaseInfoAction.php
+++ src/plugins/shared-infrastructure-DEPRECATED/src/shared-infrastructure-pack/controller-and-view/pages/env-info-pages/db-info/EnvInfoDatabaseInfoAction.php
@@ -42,11 +42,27 @@ class EnvInfoDatabaseInfoAction extends PithAction
         $migrations_database_host   = PITH_DATABASE_MIGRATIONS_DATABASE_HOST;
         $migrations_database_driver = PITH_DATABASE_MIGRATIONS_DATABASE_DRIVER;
 
+        // Get Variables using new database
+        $postgres_host          = PITH_POSTGRES_HOST;
+        $postgres_port          = PITH_POSTGRES_PORT;
+        $postgres_database_name = PITH_POSTGRES_DATABASE_NAME;
+        $postgres_username      = PITH_POSTGRES_USERNAME;
+        $postgres_password      = PITH_POSTGRES_PASSWORD;
+        $postgres_driver        = PITH_POSTGRES_DRIVER;
+        $postgres_dsn           = PITH_POSTGRES_DSN;
+
         // Push to Preparer
         $this->prepare->quote_results              = $quote_results;
         $this->prepare->dsn                        = $dsn;
         $this->prepare->migrations_database_name   = $migrations_database_name;
         $this->prepare->migrations_database_host   = $migrations_database_host;
         $this->prepare->migrations_database_driver = $migrations_database_driver;
+        $this->prepare->postgres_host              = $postgres_host;
+        $this->prepare->postgres_port              = $postgres_port;
+        $this->prepare->postgres_database_name     = $postgres_database_name;
+        $this->prepare->postgres_username          = $postgres_username;
+        $this->prepare->postgres_password          = $postgres_password;
+        $this->prepare->postgres_driver            = $postgres_driver;
+        $this->prepare->postgres_dsn               = $postgres_dsn;
     }
 }
